5db4a85cf49f12f7326bbf7cb44e93c5c6419ab7
DaniloSVJ <danilocotatoceu@gmail.com>
2024-09-27 02:10:26 -0300

    ajuste

--- templates/ListaDeMedicos/index.php
+++ templates/ListaDeMedicos/index.php
@@ -28,55 +28,23 @@
                     aria-describedby="tabela_info">
                     <thead>
                         <tr role="row">
-                            <th class="sorting_disabled" rowspan="1" colspan="1" style="width: 435px;">Nome</th>
+                            <th class="sorting_disabled" rowspan="1" colspan="1" style="width: 100px;">Nome</th>
                             <th class="esc sorting_disabled" rowspan="1" colspan="1" style="width: 107px;">CRM</th>
 
-                            <th class="sorting_disabled" rowspan="1" colspan="1" style="width: 94px;">Ações</th>
                         </tr>
                     </thead>
                     <tbody>
                        <?php foreach ($listaDeMedicos as $medicos): ?>
                         <tr class="odd">
                             <td>
-                                <input type="checkbox" id="seletor-57" class="form-check-input"
-                                    onchange="selecionar('57')">
+
                                  <?=  $medicos->nome ;?>
                             </td>
                             <td class="esc"> <?= $medicos->crm ;?></td>
 
-                            <td>
-                                <big><a href="#"
-                                        onclick="editar('57','Euclides de Souza','','','','38914417891','','','Sim','00/00/0000')"
-                                        title="Editar Dados"><i class="fa fa-edit text-primary"></i></a></big>
-
-                                <li class="dropdown head-dpdn2" style="display: inline-block;">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"
-                                        aria-expanded="false"><big><i class="fa fa-trash-o text-danger"></i></big></a>
-
-                                    <ul class="dropdown-menu" style="margin-left:-230px;">
-                                        <li>
-                                            <div class="notification_desc2">
-                                                <p>Confirmar Exclusão? <a href="#" onclick="excluir('57')"><span
-                                                            class="text-danger">Sim</span></a></p>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </li>
-
-                                <big><a href="#"
-                                        onclick="mostrar('Euclides de Souza','','','','38914417891','17/08/2024','','','Sim','00/00/0000')"
-                                        title="Mostrar Dados"><i class="fa fa-info-circle text-primary"></i></a></big>
 
 
-                                <big><a href="#" onclick="arquivo('57', 'Euclides de Souza')"
-                                        title="Inserir / Ver Arquivos"><i class="fa fa-file-o "
-                                            style="color:#22146e"></i></a></big>
 
-                                <big><a href="#" onclick="reservas('57', 'Euclides de Souza')" title="Ver Reservas"><i
-                                            class="fa fa-calendar" style="color:#827878"></i></a></big>
-
-
-                            </td>
                         </tr>
                         <?php endforeach; ?>
                         </tbody>
